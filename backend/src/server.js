a7da92caa36fd0e3021233c87d719804240d9b56
Carol <carol.tsai0224@gmail.com>
2025-03-19 11:55:02 -0400

    set up server

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// Basic API Test
+app.get('/', (req, res) => {
+    res.send('Finance Tracker API Running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
